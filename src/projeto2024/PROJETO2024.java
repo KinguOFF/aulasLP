cbe5baf9704c7ede4c970bd370f6274b8fb534a8
KinguOFF <euler.gauss.seiki@gmail.com>
2024-11-07 08:50:32 -0300

    primeiro commit

---
+++ src/projeto2024/PROJETO2024.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto2024;
+
+/**
+ *
+ * @author u09285835112
+ */
+public class PROJETO2024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
